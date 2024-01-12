ca91d5b9d4913f96346b2176ec2a76b1eaf777e0
Ye Min Naing <thargyi74@gmail.com>
2024-01-12 13:31:34 +0700

    fix for my project

--- bin/discord_bot.dart
+++ bin/discord_bot.dart
@@ -1,9 +1,9 @@
 import 'package:discord_bot/jarvis.dart';
 import 'package:discord_bot/services/chat_service.dart';
 
-void main() {
+void main() async {
   final bot = Jarvis(ChatServiceImpl());
-  bot.onInit().then((_) {
+  await bot.onInit().then((_) {
     bot.run();
   }).onError(
     (error, stackTrace) {
